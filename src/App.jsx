80e1ce730088c466036aa7c33f218a2cdc4518cc
allenn66772 <allenn4124@gmail.com>
2026-02-04 11:09:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <Home/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+   
     </>
   )
 }
